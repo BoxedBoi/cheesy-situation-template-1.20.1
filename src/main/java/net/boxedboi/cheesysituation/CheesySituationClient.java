e35f86363dc2a361bd03f5f990f9faa4861e0283
boxed <boxedboi0@gmail.com>
2025-06-23 11:33:46 +0200

    Initial commit

---
+++ src/main/java/net/boxedboi/cheesysituation/CheesySituationClient.java
@@ -0,0 +1,10 @@
+package net.boxedboi.cheesysituation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CheesySituationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/boxedboi/cheesysituation/CheesySituationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.boxedboi.cheesysituation;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CheesySituationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
